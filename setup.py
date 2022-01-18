1a60c1db7958f8b60a92cf8977c0540b925a5516
Petr Dlouhý <petr.dlouhy@email.cz>
2022-01-18 17:43:16 +0100

    Bumped version for normal release.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst', 'r') as f:
 
 setup(
     name='internet-sabotage3',
-    version='0.1.5',
+    version='0.1.6',
     description='Disable network connection for Testing',
     long_description=long_description,
     long_description_content_type='text/x-rst',
